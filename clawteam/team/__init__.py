476bec9c111277765018be483271f9648a79149a
Bumingxin <189089905+Bumingxin@users.noreply.github.com>
2026-03-24 19:16:51 +0800

    feat(final-report): guarantee final report delivery after task completion

--- clawteam/team/__init__.py
+++ clawteam/team/__init__.py
@@ -1,5 +1,6 @@
 """Team coordination layer for multi-agent collaboration."""
 
+from clawteam.team.finalizer import TeamFinalizer
 from clawteam.team.lifecycle import LifecycleManager
 from clawteam.team.mailbox import MailboxManager
 from clawteam.team.manager import TeamManager
@@ -22,4 +23,5 @@ __all__ = [
     "PlanManager",
     "LifecycleManager",
     "InboxWatcher",
+    "TeamFinalizer",
 ]
